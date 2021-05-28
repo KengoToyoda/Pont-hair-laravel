84e157841f9d9ed719645054c279c88b4dd8ae4e
KengoToyoda <toyoken1710@gmail.com>
2021-05-28 15:22:57 +0900

    メール送信機能修正

--- app/Http/Controllers/ReserveController.php
+++ app/Http/Controllers/ReserveController.php
@@ -69,7 +69,6 @@ class ReserveController extends Controller
             return redirect('/reserve/stylists/'. $user->id . '/menu=' . $menu->id)
                 ->withInput($reserves);
         } else {
-            dd($user->email);
             Mail::to($user->email)
                     ->send(new OrderShipped($reserve));
         };
